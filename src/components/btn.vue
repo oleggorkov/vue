2cfaad1d8da75bb76a480a048dcca4bb00d08c1f
Oleg <44262105+oleggorkov@users.noreply.github.com>
2020-10-29 16:07:47 +0300

    add rep

---
+++ src/components/btn.vue
@@ -0,0 +1,48 @@
+<template>
+  <button class="button">
+    <span>{{ title }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "btn",
+  props: {
+    title: {
+      type : String,
+      default: 'Купить'
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {}
+}
+</script>
+
+<style lang="scss">
+
+//@import "src/assets/styles/main";
+
+.button {
+  width: auto;
+  height: 48px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: $brown-main;
+  //color: $white;
+
+  padding: 13px 30px;
+
+  cursor: pointer;
+
+  transition: background .25s;
+
+  &:hover {
+    //background: $brown-light;
+  }
+}
+</style>
